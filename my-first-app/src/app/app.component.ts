1d552820025d60a6252e3a0a26cc27177d6ada27
Devin Patterson <dzpatterson15@gmail.com>
2023-01-21 12:39:42 -0700

    my-first-app update

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
   styles: [`
   h3{
     color: lightblue;
@@ -11,4 +11,17 @@ import { Component } from '@angular/core';
   `]
 })
 export class AppComponent {
+  serverUsername = "";
+  showSecret = false;
+  log: any = [];
+
+  onUpdateServerUsername(event: any) {
+    this.serverUsername = (<HTMLInputElement>event.target).value;
+  }
+  
+  toggleDisplay() {
+    this.showSecret = !this.showSecret;
+    this.log.push(new Date());
+  }
+  
 }
